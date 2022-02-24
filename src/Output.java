2f7e267f304d6b5285db704004625f90cef0601d
J <eri@Eri-2.local>
2022-02-25 03:05:23 +0900

    Added Output file

--- src/Output.java
+++ src/Output.java
@@ -1,8 +1,40 @@
+import java.util.List;
+
 public class Output {
 
-    // private final List<String> list;
+    private final int numOfExecutedProjects; // the number of executed projects
 
-    // Generate Constructor
+    private final List<Project> projectList;
 
-    // Generate Getters
+    public Output(int numOfExecutedProjects, List<Project> projectList) {
+        this.numOfExecutedProjects = numOfExecutedProjects;
+        this.projectList = projectList;
+    }
+
+    public int getNumOfExecutedProjects() {
+        return numOfExecutedProjects;
+    }
+
+    public List<Project> getProjectList() {
+        return projectList;
+    }
+
+    static final class Project {
+        private final String projectName; // name of the project
+
+        private final List<String> contributorNames; // names of the contributors in order of roles
+
+        public Project(String projectName, List<String> contributorNames) {
+            this.projectName = projectName;
+            this.contributorNames = contributorNames;
+        }
+
+        public String getProjectName() {
+            return projectName;
+        }
+
+        public List<String> getContributorNames() {
+            return contributorNames;
+        }
+    }
 }
